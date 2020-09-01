3442c4e6ae9ba01dfb070f12a3b2980dbebfe5d1
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-09-01 10:59:47 +0200

    Testade lite saker

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,1 +1,11 @@
-HELLO
+const express = require('express')
+const { dirname } = require('path')
+const app = express()
+const port = 1234
+
+const clientDir = __dirname + "\\client\\"
+
+app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
+app.use(express.static(clientDir));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
