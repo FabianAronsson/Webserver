3d6b7d1b6c5caa32b9505a815f17a05af957f256
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-09-03 12:59:47 +0200

    Gjorde uppgiften

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { dirname } = require('path')
+const { get } = require('http')
 const app = express()
 const port = 1234
 
 const clientDir = __dirname + "\\client\\"
 
 app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
-app.use(express.static(clientDir));
+app.get('/hanif', (req, res) => res.sendFile(clientDir + "hanif.jpg"))
+app.get('/style.css', (req, res) => res.sendFile(clientDir + "style.css"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
