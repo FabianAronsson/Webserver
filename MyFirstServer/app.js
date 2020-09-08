24cfd9e3db74d65b15f98462ae4e07f1f508def3
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-09-08 11:03:37 +0200

    Ändrade en sak

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -15,7 +15,6 @@ app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
 app.get('/hanif', (req, res) => res.sendFile(clientDir + "hanif.jpg"))
 app.get('/style.css', (req, res) => res.sendFile(clientDir + "style.css"))
 app.post('/contact', function (req, res) {
-    res.end(JSON.stringify(req.body));
     console.log(req.body)
   });
 
