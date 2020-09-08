8139e6ae301d92e577021d4046e0b75ce4ca1b44
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-09-08 11:01:42 +0200

    Gjorde upgiften

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -4,10 +4,19 @@ const { get } = require('http')
 const app = express()
 const port = 1234
 
+var bodyParser = require('body-parser')
+const { response } = require('express')
+app.use(bodyParser());
+
+
 const clientDir = __dirname + "\\client\\"
 
 app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
 app.get('/hanif', (req, res) => res.sendFile(clientDir + "hanif.jpg"))
 app.get('/style.css', (req, res) => res.sendFile(clientDir + "style.css"))
+app.post('/contact', function (req, res) {
+    res.end(JSON.stringify(req.body));
+    console.log(req.body)
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
