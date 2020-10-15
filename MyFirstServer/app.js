064d1d47f85d25f5e63c834260599d165df675dc
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-10-15 15:02:46 +0200

    Edit

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const { dirname } = require('path')
-const { get } = require('http')
 const dBModule = require ('./dBModule')
-const personModel = require('./personModel')
+//const personModel = require('./personModel')
 const messageModel = require('./messageModel')
-const { Console } = require('console')
 const app = express()
 const port = 3000
 const clientDir = __dirname + "\\client\\"
@@ -14,11 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(clientDir));
 
-app.get('/messages', async (req, res) =>{
-  res.render('pages/messages.ejs');
-}); 
-
-app.get('/', (req, res) => res.render('pages/index.ejs', { name: "", message: ""}));
+app.get('/', async (req, res) => {
+  let messages = await messageModel.getAllMessages();
+  res.render('pages/index.ejs', { message: messages})
+});
 
 /*app.post('/', function (req, res) {
     
@@ -32,7 +28,7 @@ app.get('/', (req, res) => res.render('pages/index.ejs', { name: "", message: ""
     let message = messageModel.createMessage (req.body.fname, req.body.message)
     dBModule.store(message)
     let messages = await messageModel.getAllMessages();
-    res.render('pages/messages.ejs', { message: messages})
+    res.render('pages/index.ejs', { message: messages})
   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
